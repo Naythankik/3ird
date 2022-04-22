a5edbb9fa7439d28236816364a0994359e6b6277
codeliter <hackzlord@gmail.com>
2022-04-23 00:29:57 +0100

    initial commit

---
+++ database/migrations/2022_04_22_214534_create_wish_lists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wish_lists', function (Blueprint $table) {
+                $table->id();
+                $table->unsignedBigInteger('users_id');
+                $table->unsignedBigInteger('product_id');
+                $table->timestamps();
+                $table->foreign('users_id')->references('id')->on('buyers')->cascadeOnDelete();
+                $table->foreign('product_id')->references('id')->on('products')->cascadeOnDelete();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wish_lists');
+    }
+};
